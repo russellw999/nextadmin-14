64fee88c73061e07fe2bdb86c9fa57ed81bf8085
russellw999 <russellw999@gmail.com>
2023-12-02 15:54:07 +1100

    29:00 card.jsx layout stub complete

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,8 +1,16 @@
+import { MdSupervisedUserCircle } from 'react-icons/md';
 import styles from './card.module.css'
 const Card = () => {
   return (
-    <div>
-      Card
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24}/>
+      <div className={styles.texts}>
+        <span className={styles.title}>Total Users</span>
+        <span className={styles.number}>10.928</span>
+        <span className={styles.detail}>
+            <span className={styles.positive}>12%</span> more than previous week
+        </span>            
+      </div>
     </div>
   );
 }
