97547cfddf619d896fd0efceba5ad4d645936478
russellw999 <russellw999@gmail.com>
2023-12-02 17:15:03 +1100

    52:20 stub for chart wit dummy data complete

--- app/ui/dashboard/chart/chart.jsx
+++ app/ui/dashboard/chart/chart.jsx
@@ -1,10 +1,71 @@
+'use client'
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 import styles from './chart.module.css'
 
+const data = [
+  {
+    name: 'Mon',
+    visit: 3000,
+    click: 1398,
+
+  },
+  {
+    name: 'Tue',
+    visit:2780,
+    click: 3800,
+  },
+  {
+    name: 'Wed',
+    visit: 2780,
+    click: 3908,
+  },
+  {
+    name: 'Thu',
+    visit: 1890,
+    click: 4800,
+  },
+  {
+    name: 'Fri',
+    visit: 2390,
+    click: 3800,
+  },
+  {
+    name: 'Sat',
+    visit: 3490,
+    click: 4300,
+  },
+  {
+    name: 'Sun',
+    visit: 4200,
+    click: 4560,
+  },
+];
 const Chart = () => {
   return (
-    <div>
-       Chart
+    <div className={styles.container}>
+      <h2 className={styles.title}>Weekly Recap</h2>
+      <ResponsiveContainer width="100%" height="90%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip contentStyle={{background: '#151c2c', border: 'none'}}/>
+          <Legend />
+          <Line type="monotone" dataKey="visit" stroke="#8884d8" strokeDasharray="5 5" />
+          <Line type="monotone" dataKey="click" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
