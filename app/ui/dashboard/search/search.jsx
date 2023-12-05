8a437bca49f8883f0b6ba1c600c2d29f489e9abe
russellw999 <russellw999@gmail.com>
2023-12-05 12:55:18 +1100

    02:09 search.js set url params dummy values

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -1,6 +1,21 @@
+'use client'
 import { MdSearch } from 'react-icons/md';
 import styles from './search.module.css'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 const Search = ({placeholder}) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathName = usePathname();
+
+  const params = new URLSearchParams(searchParams);
+
+  params.set("test", "value");
+
+  replace(`${pathName}?${params}`)
+
+  console.log(searchParams);
+  console.log(pathName);
+
   return (
     <div className={styles.container} >
        <MdSearch />
