6910d451b801c22f5a6f9132595c99d68869ad4a
russellw999 <russellw999@gmail.com>
2023-12-05 13:53:55 +1100

    02:14 added seearch param to Search.jsx

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -7,19 +7,32 @@ const Search = ({placeholder}) => {
   const { replace } = useRouter();
   const pathName = usePathname();
 
-  const params = new URLSearchParams(searchParams);
+  const handleSearch = (e) => {
 
-  params.set("test", "value");
+    const params = new URLSearchParams(searchParams);
 
-  replace(`${pathName}?${params}`)
+   // console.log(`e_target_value : ${e.target.value}`); 
+    if (e.target.value) {
+      e.target.value.length > 2 && params.set("q", e.target.value);
+    } else {
+      params.delete('q');
+    }
+    replace(`${pathName}?${params}`);
+   
+   // console.log(searchParams);
+   // console.log(pathName);
 
-  console.log(searchParams);
-  console.log(pathName);
+  }
+
+  
 
   return (
     <div className={styles.container} >
        <MdSearch />
-       <input type='text' placeholder={placeholder} className={styles.input}/>
+       <input type='text' 
+              placeholder={placeholder} 
+              className={styles.input}
+              onChange={handleSearch}/>
     </div>
   );
 }
